3fa8d61733705ef452054deef3d8f7022e9c8388
Giuseppe Germinaro <g.germinaro88@gmail.com>
2018-06-05 12:55:37 +0200

    Added styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,17 @@
 import React from 'react'
+import styled from 'styled-components'
 
-export default () => <div>Hello Andshape!</div>
+const Container = styled.div`
+  margin: 3rem auto;
+  max-width: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+export default () => (
+  <Container>
+    <div>Hello Andshape!</div>
+  </Container>
+)
