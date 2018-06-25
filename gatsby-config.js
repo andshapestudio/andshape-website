e20daea7b72125bcfa02df513506fee69ad92f7d
Giuseppe Germinaro <g.germinaro88@gmail.com>
2018-06-25 12:06:54 +0200

    Fixing typography plugin in production

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,11 @@
 module.exports = {
   plugins: [
-    `gatsby-plugin-typography`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`
+      }
+    },
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`
   ]
